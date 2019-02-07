228181c66561a269137edff16cbde64b16cf91c0
bstrobel84@gmail.com <>
2019-02-07 10:57:19 -0500

    Brandon Strobel added a method.

---
+++ src/collaboration/CollabMain.java
@@ -0,0 +1,16 @@
+
+package collaboration;
+
+public class CollabMain {
+
+    public static void main(String[] args) {
+        //DO NOT EDIT ADD ONE METHOD CALL
+        BrandonStrobel();
+    }
+    /*Create methods 
+     *declaring you have made a change mine is below
+    */
+    public static void BrandonStrobel(){
+        System.out.println("Brandon Strobel.");
+    }
+}
