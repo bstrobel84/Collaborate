beeecdfb893cd804e11c9214eacf162e03b5b59f
jmm87@students.ptcollege.edu <Jonathan Morales@JonathansLaptop>
2019-02-21 11:50:01 -0500

    Changed the program to repeat until the word is correctly guessed.

--- src/collaboration/Project2.java
+++ src/collaboration/Project2.java
@@ -9,12 +9,17 @@ public class Project2 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         String userInput;
+        boolean control = true;
+        while(control){
         System.out.print("Enter a word>>");
         userInput = input.nextLine();
         if(userInput.equalsIgnoreCase(WORD)){
-            System.out.println("Good job you guessed the word>>");
-        }else{
+            System.out.println("Good job you guessed the word!!");
+            control = false;
+        }
+        else{
             System.out.println("You didnt guess the correct word please try again.");
+        }
         }
     }
 }
